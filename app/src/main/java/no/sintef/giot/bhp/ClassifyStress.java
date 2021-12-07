01c99e60e66af9894db848017bf56844bb65c188
rdautov <dautov.rustem@gmail.com>
2021-12-07 14:00:44 +0100

    Code cleanup + Checkstyle added

---
+++ app/src/main/java/no/sintef/giot/bhp/ClassifyStress.java
@@ -0,0 +1,14 @@
+package no.sintef.giot.bhp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClassifyStress extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_classify_stress);
+  }
+}
+
